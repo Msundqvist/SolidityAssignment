f77f8ce4d2be13f6e528d59b0af88b56febd144c
MatildaSundqvist <matildasundqvist@live.se>
2025-09-10 16:14:16 +0200

    changed mapping and name to subcription

--- contracts/Prenumeration.sol
+++ contracts/Prenumeration.sol
@@ -3,26 +3,40 @@ pragma solidity ^0.8.28;
 
 contract Prenumeration{
 
+    address private owner;
     enum PrenumerationState { Paused, Ongoing, Ended}
 
-    struct PrenItem{
-        uint id;
-        uint duration;
-        uint price;
-        address owner;
-        string prenumerationTitle;
-        bool isActive;
+    struct Subscribe{
+        string title;
+        uint16 duration;
+        uint16 price;
+        bool exists;
 
     }
 
-
+  mapping(string => Subscribe[]) public subscriptions;
 
 
     PrenumerationState public prenumerationState;
 
-
-    constructor {
-      
-        }
+    modifier inState (PrenumerationState state){
+        require(prenumerationState == state, "Invalid state, Action cannot be preformed!");
+        _;
     }
 
+
+
+    constructor (){
+        owner = msg.sender;
+    } 
+
+    function createPren(string memory subscriptionTitle, string memory title, uint16 duration, uint16 price)  public{
+        subscriptions[subscriptionTitle].push(Subscribe({
+            title: title,
+            duration: duration,
+            price: price,
+            exists: true
+        }));
+    }                                                                                    
+
+}
