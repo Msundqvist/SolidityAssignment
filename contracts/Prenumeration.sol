c38733d85234cf9abee48ddca0a1e102ad18c000
MatildaSundqvist <matildasundqvist@live.se>
2025-09-10 19:04:55 +0200

    created event for subscriptionCreated, fixed errors

--- contracts/Prenumeration.sol
+++ contracts/Prenumeration.sol
@@ -1,52 +1,58 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.28;
 
-contract Prenumeration{
+contract Prenumeration {
 
     address private owner;
-    enum SubscriptionState { Paused, Ongoing, Ended}
+    enum SubscriptionState { Paused, IsActive }
 
-    struct Subscribe{
+    struct Subscribe {
         string title;
         uint16 duration;
         uint16 price;
         bool exists;
-
     }
 
-  mapping(string => Subscribe[]) public subscriptions;
-
+    mapping(string => Subscribe[]) public subscriptions;
 
     SubscriptionState public subscriptionState;
 
-    modifier inState (PrenumerationState state){
-        require(prenumerationState == state, "Invalid state, Action cannot be preformed!");
+    modifier inState(SubscriptionState state) {
+        require(subscriptionState == state, "Invalid state, Action cannot be performed!");
         _;
     }
 
-    modifier onlyOwner(){
-        require(msg.sender == owner, "Only owner can call this function!!")
-        
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function!!");
+        _;
     }
 
-
-
-    constructor (){
+    constructor() {
         owner = msg.sender;
-        subscriptions = SubscriptionState.Paused;
-    } 
-
-    function setState(SubscriptionState state) public onlyOwner{
-        subscriptionState = state; 
+        subscriptionState = SubscriptionState.Paused;
     }
 
-    function createSubscription(string memory subscriptionTitle, string memory title, uint16 duration, uint16 price)  public onlyOwner inState(subscriptionState.Ongoing){
-        subscriptions[subscriptionTitle].push(Subscribe({
+    event SubscriptionCreated(string indexed subscriptionTitle, uint16 duration, uint16 price);
+
+    function setState(SubscriptionState state) public onlyOwner {
+        subscriptionState = state;
+    }
+
+    function createSubscription(
+        string memory title,
+        uint16 duration,
+        uint16 price
+    )
+        public
+        onlyOwner
+        inState(SubscriptionState.IsActive)
+    {
+        subscriptions[title].push(Subscribe({
             title: title,
             duration: duration,
             price: price,
             exists: true
         }));
-    }                                                                                    
-
+        emit SubscriptionCreated(title, duration, price);
+    }
 }
